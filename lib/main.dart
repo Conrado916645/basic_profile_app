ac231010ab634e29e2a255053fa59c4662f1ebce
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-04-23 10:52:24 +0800

    change tex font family

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,11 @@ class MyApp extends StatelessWidget {
                   'https://scontent.fmnl18-1.fna.fbcdn.net/v/t1.0-9/p960x960/82822916_456001548617228_6769033333700558848_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_eui2=AeFU59oKfPBO8qydTlo3NKV57oMzDII2ndnugzMMgjad2dabV8p6eGYkDmMKIJfhfIuLz6NOPEpU0x05BBEP1A6U&_nc_ohc=YJ37QAMV1h4AX9ge3Qk&_nc_ht=scontent.fmnl18-1.fna&_nc_tp=6&oh=664f8cfd196cbc35b215c8b6477c0463&oe=5EC45FFF'),
             ),
             Text(
-              'Conrado N. Agravio III',
-              style: TextStyle(color: Colors.white, fontSize: 30.0),
+              'Juan B. Dela Cruz',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontFamily: 'Schoolbell'),
             ),
             SizedBox(
               width: 300.0,
@@ -35,7 +38,7 @@ class MyApp extends StatelessWidget {
             Text(
               'COMPUTER PROGRAMMER',
               style: TextStyle(
-                  color: Colors.white60, fontSize: 20.0, letterSpacing: 2.5),
+                  color: Colors.white60, fontSize: 20.0, letterSpacing: 2.5,fontFamily: 'SourceSansPro'),
             ),
             Container(
               margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
